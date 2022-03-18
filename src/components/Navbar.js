82d69b28a52311c2e69184a4a5d23f6870e13ce9
Delvoid <delvoidltd@gmail.com>
2022-03-18 21:02:57 +0000

    navbar links to only show on home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,10 @@
 import Search from './Search'
+import { useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const location = useLocation()
+
   return (
     <header className="border-b border-gray-800 sticky top-0 z-10 backdrop-blur-md">
       <nav className="container mx-auto flex flex-col lg:flex-row items-center justify-between px-4 py-6">
@@ -13,26 +17,36 @@ const Navbar = () => {
             />
           </a>
           <ul className="flex ml-0 lg:ml-16 space-x-8 mt-6 lg:mt-0">
-            <li>
-              <a href="#popular" className="hover:text-gray-400">
-                Popular
-              </a>
-            </li>
-            <li>
-              <a href="#now-playing" className="hover:text-gray-400">
-                Now Playing
-              </a>
-            </li>
-            <li>
-              <a href="#coming-soon" className="hover:text-gray-400">
-                Coming Soon
-              </a>
-            </li>
-            <li>
-              <a href="#top-rated" className="hover:text-gray-400">
-                Top Rated
-              </a>
-            </li>
+            {location.pathname === '/' ? (
+              <>
+                <li>
+                  <a href="#popular" className="hover:text-gray-400">
+                    Popular
+                  </a>
+                </li>
+                <li>
+                  <a href="#now-playing" className="hover:text-gray-400">
+                    Now Playing
+                  </a>
+                </li>
+                <li>
+                  <a href="#coming-soon" className="hover:text-gray-400">
+                    Coming Soon
+                  </a>
+                </li>
+                <li>
+                  <a href="#top-rated" className="hover:text-gray-400">
+                    Top Rated
+                  </a>
+                </li>
+              </>
+            ) : (
+              <li>
+                <Link to={'/'} className="hover:text-gray-400">
+                  Home
+                </Link>
+              </li>
+            )}
           </ul>
         </div>
         <div className="flex items-center mt-6 lg:mt-0">
